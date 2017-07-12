bbc9802a50c1bf1a56aa101d3f01b1af4c77162d
dpattinson <dpattinson@192.168.140.192>
2017-07-12 17:32:33 +0100

    Fixed/completed the server re-registration logic.

--- src/main/java/uk/co/simplyhealth/ServerRegistrationComponent.java
+++ src/main/java/uk/co/simplyhealth/ServerRegistrationComponent.java
@@ -1,21 +1,31 @@
 package uk.co.simplyhealth;
 
-import java.util.Map;
+import java.io.File;
 
 import org.mule.api.MuleEventContext;
 import org.mule.api.lifecycle.Callable;
+import org.springframework.beans.factory.annotation.Value;
 
 public class ServerRegistrationComponent implements Callable {
 
+	@Value("${server.name}")
+	private String serverName;
+	
 	@Override
 	public Object onCall(MuleEventContext eventContext) throws Exception {
 		
+		String registrationToken = eventContext.getMessage().getPayloadAsString();
+		
 		ProcessBuilder processBuilder = new ProcessBuilder();
-		processBuilder.command("", "", "");
+		
+		// ./amc_setup --encrypt --hybrid $ANYPOINT_PLATFORM_REGISTRATION_TOKEN mule-container-test
+		processBuilder.command("/opt/mule/bin/amc_setup", "--encrypt", "--hybrid", registrationToken, serverName);
+
+		processBuilder.directory(new File("/opt/mule/bin"));
 		processBuilder.inheritIO();
 		
-		Map<String, String> processEnvironment = processBuilder.environment();
-		processEnvironment.put("MULE_HOME", "");
+//		Map<String, String> processEnvironment = processBuilder.environment();
+//		processEnvironment.put("MULE_HOME", "");
 		
 		Process process = processBuilder.start();
 		
